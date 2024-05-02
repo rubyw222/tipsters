3e8d09eed26dafacb8802482ebbff56fa65f37c5
Ruby Wood <rwood@thelumery.com>
2024-05-02 14:17:24 +1000

    header

--- src/app.js
+++ src/app.js
@@ -1,26 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
+import { Layout, Flex } from 'antd';
 
 // components
 import Ladder from "./components/ladder/ladder";
 import Account from "./components/account/account";
 import Dashboard from "./components/dashboard/dashboard";
 import Login from "./components/login";
-import NavBar from "./components/navbar";
 import NotFound from './components/notFound';
+import Structure from "./components/structure";
+
+const { Header, Footer, Content } = Layout;
+
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+};
 
 const App = () => (
-  <>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Ladder/>} />
-      <Route path="/account" element={<Account/>} />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </>
+    <Flex gap="middle" wrap="wrap">
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>
+        <Routes>
+          <Route path="/" element={<Ladder/>} />
+          <Route path="/account" element={<Account/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  </Flex>
 );
 
 export default App;
---
+++ src/components/structure.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Layout, Flex } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+};
+
+const Structure = () => (
+  <Flex gap="middle" wrap="wrap">
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  </Flex>
+);
+
+export default Structure;
